8425154e24cbdea7efcd43fcd6a9b99eddd4c833
Chunho Park <faterider@naver.com>
2017-02-06 18:15:49 +0900

    Add imagefiles and Update

--- main.js
+++ main.js
@@ -4,10 +4,25 @@ document.addEventListener("DOMContentLoaded", function(){
 function init(){
 	var navi = document.querySelector(".navi");
 	var service = document.querySelector(".service");
+	var searchBar = document.querySelector(".search_bar");
+	var inputBox = document.querySelector(".search_input");
 	navi.addEventListener("click", function(evt){
 		if(evt.target.id === "view_more"){
 			service.classList.toggle("hidden");
 		}
+	});
+	service.addEventListener("click", function(evt){
+		if(evt.target.tagName !== "BUTTON") return;
+		if(evt.target.id === "nav_close") {
+			service.classList.toggle("hidden");
+		}
+	});
+	inputBox.addEventListener("focus", function(){
+		searchBar.classList.toggle("search_on");
+		
+	});
+	inputBox.addEventListener("focusout", function(){
+		searchBar.classList.toggle("search_on");
 	});
 }
 
