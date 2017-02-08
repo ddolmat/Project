6b6990bb5348dc445eb97d2d4652d88170168bf0
Chunho Park <faterider@naver.com>
2017-02-08 18:49:21 +0900

    Refactoring tabUI main.js fix markup daum.html

--- main.js
+++ main.js
@@ -6,25 +6,33 @@ function init(){
 	var service = document.querySelector(".service");
 	var searchBar = document.querySelector(".search_bar");
 	var inputBox = document.querySelector(".search_input");
-	var mediaTabWrap = document.querySelector(".mediaTab_wrap");
+	var tabWraps = {
+		media:document.querySelector(".mediaTab_wrap"),
+		fun:document.querySelector(".funGrpTab_wrap")	
+	};
+	var tabInfos = {
+		media:setSlidePosition(tabWraps.media),
+		fun:setSlidePosition(tabWraps.fun)
+	};
+	var btnID = {
+		media: {
+			prev:"mediaPrevBtn",
+			next:"mediaNextBtn"
+		},
+		fun:{
+			prev:"funGrpPrevBtn",
+			next:"funGrpNextBtn"
+		}
+	};
+
 	var flag = {done:true};
-	var mediaTabInfo = setSlidePosition(mediaTabWrap);
 	document.addEventListener("transitionend", function(){
 		flag.done = true;
 	});
 	document.addEventListener("click", function(evt){
 		if(flag.done === false) return;
 		flag.done = false;
-		if(evt.target.id === "mediaPrevBtn"){
-			var mediaTabX = +mediaTabWrap.style.transform.replace(/translate3d\((-?\d+).+/, "$1");
-			moveLeft(mediaTabWrap, mediaTabX, mediaTabInfo);
-			return;
-		}
-		if(evt.target.id === "mediaNextBtn"){
-			var mediaTabX = +mediaTabWrap.style.transform.replace(/translate3d\((-?\d+).+/, "$1");
-			moveRight(mediaTabWrap, mediaTabX, mediaTabInfo);
-			return;
-		}
+		selectAction(evt.target.id, tabWraps, btnID, tabInfos);
 		flag.done = true;
 		if(evt.target.id === "view_more"){
 			service.classList.toggle("hidden");
@@ -44,37 +52,62 @@ function init(){
 	});
 
 }
-function moveLeft(div, x, info){
+function insertX(wrap){
+	return +wrap.style.transform.replace(/translate3d\((-?\d+).+/, "$1");	
+}
+function selectAction(id, tabWraps, btnID, tabInfos){
+	var x = {
+		media:insertX(tabWraps.media),
+		fun:insertX(tabWraps.fun)
+	};
+	if(id === btnID.media.prev){
+		moveLeft(tabWraps.media, x.media, tabInfos.media);
+		return;
+	}
+	if(id === btnID.media.next){ 
+		moveRight(tabWraps.media, x.media, tabInfos.media);
+		return;
+	}
+	if(id === btnID.fun.prev){ 
+		moveLeft(tabWraps.fun, x.fun, tabInfos.fun);
+		return;
+	}
+	if(id === btnID.fun.next){ 
+		moveRight(tabWraps.fun, x.fun, tabInfos.fun);
+		return;
+	}
+}
+function moveLeft(slideWrap, x, info){
 	if(x === info.width){
 		var mod =  - (info.nums - 1) * info.width;
 		var mod2 = mod + info.width;
-		setTrans(div, mod, "none");
+		setTrans(slideWrap, mod, "none");
 		setTimeout(function(){
-			setTrans(div, mod2, "All 0.6s, ease-in-out");
+			setTrans(slideWrap, mod2, "All 0.6s, ease-in-out");
 		}, 10);
 		return;
 	}
 	x += info.width;
-	setTrans(div, x, "");
+	setTrans(slideWrap, x, "");
 	return;
 }
 
-function moveRight(div, x, info){
+function moveRight(slideWrap, x, info){
 	var a = info.nums * info.width;
 	if(x === -a) {
-		setTrans(div, 0, "none");
+		setTrans(slideWrap, 0, "none");
 		setTimeout(function(){
-			setTrans(div, -info.width, "All 0.6s, ease-in-out");		
+			setTrans(slideWrap, -info.width, "All 0.6s, ease-in-out");		
 		}, 10);
 		return;	
 		}
 	x -= info.width;
-	setTrans(div, x, "");
+	setTrans(slideWrap, x, "");
 	return;	
 }
 
 function setSlidePosition(slideWrap){
-	var wrapWidth = parseInt(window.getComputedStyle(slideWrap).width);
+	var wrapWidth = parseInt(window.getComputedStyle(slideWrap.parentNode).width);
 	var nums = slideWrap.children.length;
 	setTrans(slideWrap, 0, "");
 	for(var i = 0; i < nums; i++){
@@ -86,16 +119,16 @@ function setSlidePosition(slideWrap){
 			nums:nums
 	};
 }
-function insertClone(div, width){
-	var len = div.children.length;
-	var lastClone = div.children[len-1].cloneNode(true);
+function insertClone(slideWrap, width){
+	var len = slideWrap.children.length;
+	var lastClone = slideWrap.children[len-1].cloneNode(true);
 	var lastX = -width;
 	setTrans(lastClone, lastX, "");
-	div.insertBefore(lastClone, div.children[0]);
-	var firstClone = div.children[1].cloneNode(true);
+	slideWrap.insertBefore(lastClone, slideWrap.children[0]);
+	var firstClone = slideWrap.children[1].cloneNode(true);
 	var firstX = width * len;
 	setTrans(firstClone, firstX, "");
-	div.appendChild(firstClone);
+	slideWrap.appendChild(firstClone);
 }
 function setTrans(node, x, tr){
 	node.style.transform = "translate3d("+x+"px, 0px, 0px)";
